0870d9ae9f1ccec1d2a4f827ad6dab28c926c773
Hanif yogatama <hanifyotama@gmail.com>
2020-07-24 02:27:59 +0700

    add menu

--- app/src/main/java/com/binar/designimplementationnimblapp/MainActivity.kt
+++ app/src/main/java/com/binar/designimplementationnimblapp/MainActivity.kt
@@ -2,10 +2,16 @@ package com.binar.designimplementationnimblapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        this.supportActionBar?.hide()
+//        Glide.with(this)
+//            .load(R.drawable.oppa)
+//            .circleCrop()
+//            .into(ivStory1)
     }
 }
